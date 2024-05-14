db9e7b29e7704f247478ada4086910cd3f1f99b5
Abin Antony <abina5448@gmail.com>
2024-05-14 15:13:06 +0530

    page

--- app/(my_kuttanad)/pages/layout.tsx
+++ app/(my_kuttanad)/pages/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Layout({children}:{children:React.ReactNode}){
     return <main className="min-h-[100vh]">
         <div className="w-full p-3 flex justify-between">
-            <Image src={'/assets/app/app_logo.png'} alt="My Kuttanad Logo" />
+            <Image src={'/assets/app/app_logo.png'} width={60} height={60} alt="My Kuttanad Logo" />
         </div>
         {children}
     </main>   
