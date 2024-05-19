b4897c9617f4d95e2b25dc413b07110b3c1e9f5d
Abin Antony <abina5448@gmail.com>
2024-05-19 23:49:13 +0530

    images

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,16 @@ import { withNextVideo } from "next-video/process";
 
 export const  nextConfig = {
     reactStrictMode: true,
-    remotePatterns: [
+    images: {
+      remotePatterns: [
         {
           protocol: "https", // or http
-          hostname: "mykuttanadu.s3.us-west-1.amazonaws.com", // if your website has no www, drop it
+          hostname: "mykuttanadu.s3.us-west-1.amazonaws.com",
+          port:'',
+          pathname:'/**'
         },
       ],
+    }
 };
 
 export default withNextVideo(nextConfig, { folder: 'public/videos' });
