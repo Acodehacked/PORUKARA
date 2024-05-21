07d80761ad3337fbc1101827e04d9defb9c4e547
Abin Antony <abina5448@gmail.com>
2024-05-21 12:01:09 +0530

    main

--- app/api/webcode/form/route.ts
+++ app/api/webcode/form/route.ts
@@ -6,37 +6,41 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
     const data = await req.json();
     const { db, connection } = await getDb2();
-    const sendMail = () =>{
-        fetch('https://script.google.com/macros/s/AKfycbyn6esA4BhK4xaAePSfKViXulOZWvswg_vPOj-qiZOwkp83HMKDQMO9Izo_U1u1mVP2/exec',{
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
+        try {
+            const response = await db.insert(WebcodeFormTable).values({
                 name:data.name,
                 phone:data.phone,
-                recipient:data.mail
-            })
-        });
-    } 
-    if (data != null) {
-        const response = await db.insert(WebcodeFormTable).values({
-            name:data.name,
-            phone:data.phone,
-            email:data.mail,
-            type:data.type,
-            message:data.message,
-        });
-        connection.end();
-        sendMail();
+                email:data.mail,
+                type:data.type,
+                message:data.message,
+            });
+            connection.end();
+        } catch (error) {
+            return NextResponse.json({
+                data: 'not send added to database',
+                error: null
+            });
+        }
+        try {
+            fetch('https://script.google.com/macros/s/AKfycbyn6esA4BhK4xaAePSfKViXulOZWvswg_vPOj-qiZOwkp83HMKDQMO9Izo_U1u1mVP2/exec',{
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name:data.name,
+                    phone:data.phone,
+                    recipient:data.mail
+                })
+            });
+        } catch (error) {
+            return NextResponse.json({
+                data: 'not send email',
+                error: null
+            });
+        }
         return NextResponse.json({
-            data: response,
+            data: 'no error',
             error: null
         });
-    }
-    connection.end();
-    return NextResponse.json({
-        data: data,
-        error: 'not submitted'
-    });
 }
