d96b8f918b6538267ea2cd6d448b981a789fdf1a
Abin Antony <abina5448@gmail.com>
2024-05-21 10:51:52 +0530

    adding

--- app/api/webcode/form/route.ts
+++ app/api/webcode/form/route.ts
@@ -1,19 +1,30 @@
 'use server'
+import { getDb2 } from "@/db";
+import { WebcodeFormTable } from "@/db/schema";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const data = await req.json();
-    // const { db, connection } = await getDb2();
-    // const id = data.value;
-    // var categories;
-    // if(id > 0){
-        // categories = await db.select().from(app_place).where(eq(app_place.app_category_id,id));
-    // }else{
-        // categories = await db.select().from(app_place).limit(50);
-    // }
-    // connection.end();
+    const { db, connection } = await getDb2();
+    const id = data.value;
+    var categories;
+    if (data != null) {
+        const response = await db.insert(WebcodeFormTable).values({
+            name:data.name,
+            phone:data.phone,
+            email:data.mail,
+            type:data.type,
+            message:data.message,
+        });
+        connection.end();
+        return NextResponse.json({
+            data: response,
+            error: null
+        });
+    }
+    connection.end();
     return NextResponse.json({
         data: data,
-        error: null
+        error: 'not submitted'
     });
 }
