501269baea19e154984bc6d1dba433e7563682a3
Abin Antony <abina5448@gmail.com>
2024-05-21 10:39:33 +0530

    json structed

--- components/reusable/JsonStructured.tsx
+++ components/reusable/JsonStructured.tsx
@@ -1,4 +1,4 @@
-const JsonLd = ({ data }) => (
+const JsonLd = ({ data }:{data:object}) => (
     <script
       type="application/ld+json"
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
