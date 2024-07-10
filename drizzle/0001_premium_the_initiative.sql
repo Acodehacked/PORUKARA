cb9b9d15dd55830b87b19bc89d35cc7449319843
Abin Antony <abina5448@gmail.com>
2024-07-10 17:24:23 +0530

    add

--- drizzle/0001_premium_the_initiative.sql
+++ drizzle/0001_premium_the_initiative.sql
@@ -1,5 +1,10 @@
 ALTER TABLE `app_place` MODIFY COLUMN `working_days` json;--> statement-breakpoint
 ALTER TABLE `app_place` MODIFY COLUMN `opening_time` json;--> statement-breakpoint
+ALTER TABLE `app_place` ADD `paid` boolean DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE `app_place` ADD `period` int DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE `app_place` ADD `end_date` date DEFAULT '2024-07-10';--> statement-breakpoint
+ALTER TABLE `app_place` ADD `google_location` varchar(1000) NOT NULL;--> statement-breakpoint
+ALTER TABLE `app_place` ADD `pincode` int;
 -- DROP TABLE `app_panchayats`;--> statement-breakpoint
 -- DROP TABLE `app_type_categories`;--> statement-breakpoint
 -- ALTER TABLE `app_place` MODIFY COLUMN `place` varchar(300) NOT NULL;--> statement-breakpoint
@@ -7,8 +12,3 @@ ALTER TABLE `app_place` MODIFY COLUMN `opening_time` json;--> statement-breakpoi
 -- ALTER TABLE `app_place` MODIFY COLUMN `ward_no` int;--> statement-breakpoint
 -- ALTER TABLE `app_place` MODIFY COLUMN `website` varchar(100);--> statement-breakpoint
 -- ALTER TABLE `app_categories` ADD `sub_categories` json DEFAULT ('[]') NOT NULL;--> statement-breakpoint
--- ALTER TABLE `app_place` ADD `paid` boolean DEFAULT false NOT NULL;--> statement-breakpoint
--- ALTER TABLE `app_place` ADD `period` int DEFAULT 0 NOT NULL;--> statement-breakpoint
--- ALTER TABLE `app_place` ADD `end_date` date DEFAULT '2024-07-10';--> statement-breakpoint
--- ALTER TABLE `app_place` ADD `google_location` varchar(1000) NOT NULL;--> statement-breakpoint
--- ALTER TABLE `app_place` ADD `pincode` int;
