9ccaba00e18346898f32e256e5c11549cd4965af
Abin Antony <abina5448@gmail.com>
2024-07-13 13:17:45 +0530

    async

--- app/api/app/auth/VerifyEmail/route.ts
+++ app/api/app/auth/VerifyEmail/route.ts
@@ -1,13 +1,9 @@
 'use server'
 import nodemailer from 'nodemailer'
-import * as SibApiV3Sdk from '@sendinblue/client'
 import { getDb2 } from "@/db"
-import * as crypto from 'crypto';
 import { app_logintable } from "@/db/schema";
 import { eq } from "drizzle-orm";
-import { Code } from 'lucide-react';
 import { NextResponse } from 'next/server';
-import { error } from 'console';
 
 export async function GET(request: Request) {
     
@@ -59,8 +55,17 @@ export async function GET(request: Request) {
     }
 }
 
+function generateOTP({ length }: { length: number }) {
+    let digits = '0123456789';
+    let OTP = '';
+    let len = digits.length
+    for (let i = 0; i < length; i++) {
+        OTP += digits[Math.floor(Math.random() * len)];
+    }
 
-async function SendVerificationMail({ mail, code }: { mail?: string, code?: string }) {
+    return OTP;
+}
+export async function SendVerificationMail({ mail, code }: { mail?: string, code?: string }) {
 
     let transporter = nodemailer.createTransport({
         host: "smtp-relay.brevo.com",
@@ -73,45 +78,71 @@ async function SendVerificationMail({ mail, code }: { mail?: string, code?: stri
     });
 
     // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: 'porukaracollege@gmail.com', // sender address
-        to: [`${mail}`, 'porukaracollege@gmail.com'], // list of receivers
-        subject: "Verification Code - My Kuttanad App", // Subject line
-        html: `<html><head>
-                <style>
-                    .muted{
-                        color: grey
-                    }
-                </style>
-            </head><body>
-            <img src="https://porukaracollege.in/assets/app/app_logo.png"  width="60px"  /><h3></h3>
-            
-            <p>Hi ${mail},</p>
-            <p>Greetings from My Kuttanad App</p>
-            <br>
-            <h2>Your Verification Code is ${code ?? ''}</h2>
-            <br>
-            <p>Please use this code to verify signup / login in My Kuttanad App</p>
-            <br>
-            <p>If you didn't request this code, you can safely ignore this mail. Someone else might have typed your mail address by mistake.</p>
-            <br>
-            <br>
-            <br>
-            <br>
-            <p>Thanks, My Kuttanad App Developer</p>
-            <p>Abin Antony Kattady</p>
-            <p>2024 all rights reserved &copy; mykuttanad</p>
-            </body></html>`,
-    });
-    console.log('sent : ' + info.messageId)
+    setTimeout(async ()=>{
+        let info = await transporter.sendMail({
+            from: 'porukaracollege@gmail.com', // sender address
+            to: [`${mail}`, 'porukaracollege@gmail.com'], // list of receivers
+            subject: "Verification Code - My Kuttanad App", // Subject line
+            html: `<html><head>
+                    <style>
+                        .muted{
+                            color: grey
+                        }
+                    </style>
+                </head><body>
+                <img src="https://porukaracollege.in/assets/app/app_logo.png"  width="60px"  /><h3></h3>
+                
+                <p>Hi ${mail},</p>
+                <p>Greetings from My Kuttanad App</p>
+                <br>
+                <h2>Your Verification Code is ${code ?? ''}</h2>
+                <br>
+                <p>Please use this code to verify signup / login in My Kuttanad App</p>
+                <br>
+                <p>If you didn't request this code, you can safely ignore this mail. Someone else might have typed your mail address by mistake.</p>
+                <br>
+                <br>
+                <br>
+                <br>
+                <p>Thanks, My Kuttanad App Developer</p>
+                <p>Abin Antony Kattady</p>
+                <p>2024 all rights reserved &copy; mykuttanad</p>
+                </body></html>`,
+        });
+        console.log('sent : ' + info.messageId)
+    },400);
 }
-function generateOTP({ length }: { length: number }) {
-    let digits = '0123456789';
-    let OTP = '';
-    let len = digits.length
-    for (let i = 0; i < length; i++) {
-        OTP += digits[Math.floor(Math.random() * len)];
-    }
 
-    return OTP;
-}
+
+
+
+
+ 
+// async function getCookieData() {
+//   return new Promise((resolve) =>
+//     setTimeout(() => {
+//       // cookies will be called outside of the async context, causing a build-time error
+//       resolve(cookies().getAll())
+//     }, 1000)
+//   )
+// }
+ 
+// export default async function Page() {
+//   const cookieData = await getCookieData()
+//   return <div>Hello World</div>
+// }
+
+ 
+// async function getCookieData() {
+//   const cookieData = cookies().getAll()
+//   return new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve(cookieData)
+//     }, 1000)
+//   )
+// }
+ 
+// export default async function Page() {
+//   const cookieData = await getCookieData()
+//   return <div>Hello World</div>
+// }
