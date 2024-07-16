7598a2fa55cab2f6af9ddd088b5addb56e2fb374
Abin Antony <abina5448@gmail.com>
2024-07-16 17:34:42 +0530

    drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,15 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config();
-export default {
+export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: 'mysql2',
-  dbCredentials: {
-    user: "root",
-    password: 'Abin@12345',
-    host: "localhost",
-    port: 3306,
-    database: "porukaracollege",
-  }
-} satisfies Config;
+  breakpoints: true,
+  dbCredentials: ({
+      user: "root" ?? '',
+      password: 'Abin@12345',
+      host: "localhost",
+      port: 3306,
+      database: "porukaracollege",
+  })
+});
