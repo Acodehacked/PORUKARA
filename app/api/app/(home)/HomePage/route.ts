29935f0f6da99b7f7037f205eac9fe72c7c811ca
Abin Antony <abina5448@gmail.com>
2024-07-26 21:42:06 +0530

    homepage api

--- app/api/app/(home)/HomePage/route.ts
+++ app/api/app/(home)/HomePage/route.ts
@@ -29,16 +29,16 @@ export async function GET(request: NextRequest) {
         }else{
             var interest_places = [];
             var interest_categories:number[] = [];
-            user?.categories.forEach(async (cat,index)=> {
+            // user?.categories.forEach(async (cat,index)=> {
                 
-                const ry = await db.select().from(app_top_categories).where(eq(app_top_categories.id,cat));
-                if(ry.length > 0){
-                    ry[0].subCategories.forEach((mcate)=>{
-                        interest_categories.push(mcate);
-                    })
-                }
-                // interest_categories.push()
-            })
+            //     const ry = await db.select().from(app_top_categories).where(eq(app_top_categories.id,cat));
+            //     if(ry.length > 0){
+            //         ry[0].subCategories.forEach((mcate)=>{
+            //             interest_categories.push(mcate);
+            //         })
+            //     }
+            //     // interest_categories.push()
+            // })
             const topcate = await db.select().from(app_top_categories);
             const categ = await db.select().from(app_categories).limit(50);
             const top_places = await db.select().from(app_place).where(gt(app_place.rating,4)).limit(10);
@@ -72,65 +72,6 @@ export async function GET(request: NextRequest) {
         });
       }
 }
-
-function generateOTP({ length }: { length: number }) {
-    let digits = '0123456789';
-    let OTP = '';
-    let len = digits.length
-    for (let i = 0; i < length; i++) {
-        OTP += digits[Math.floor(Math.random() * len)];
-    }
-
-    return OTP;
-}
-async function SendVerificationMail({ mail, code }: { mail?: string, code?: string }) {
-
-    let transporter = nodemailer.createTransport({
-        host: "smtp-relay.brevo.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: "77d555001@smtp-brevo.com", // generated ethereal user
-            pass: "9wKmVZG24zFk0a8y", // generated ethereal password
-        },
-    });
-
-    // send mail with defined transport object
-    setTimeout(async ()=>{
-        let info = await transporter.sendMail({
-            from: 'porukaracollege@gmail.com', // sender address
-            to: [`${mail}`, 'porukaracollege@gmail.com'], // list of receivers
-            subject: "Verification Code - My Kuttanad App", // Subject line
-            html: `<html><head>
-                    <style>
-                        .muted{
-                            color: grey
-                        }
-                    </style>
-                </head><body>
-                <img src="https://porukaracollege.in/assets/app/app_logo.png"  width="60px"  /><h3></h3>
-                
-                <p>Hi ${mail},</p>
-                <p>Greetings from My Kuttanad App</p>
-                <br>
-                <h2>Your Verification Code is ${code ?? ''}</h2>
-                <br>
-                <p>Please use this code to verify signup / login in My Kuttanad App</p>
-                <br>
-                <p>If you didn't request this code, you can safely ignore this mail. Someone else might have typed your mail address by mistake.</p>
-                <br>
-                <br>
-                <br>
-                <br>
-                <p>Thanks, My Kuttanad App Developer</p>
-                <p>Abin Antony Kattady</p>
-                <p>2024 all rights reserved &copy; mykuttanad</p>
-                </body></html>`,
-        });
-        console.log('sent : ' + info.messageId)
-    },400);
-}
-
 
 
 
