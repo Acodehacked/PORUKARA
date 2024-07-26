9c15010233ce550c1d82840bfc9e5bf57cf1a967
Abin Antony <abina5448@gmail.com>
2024-07-26 22:07:55 +0530

    home api updated

--- app/api/app/(home)/HomePage/route.ts
+++ app/api/app/(home)/HomePage/route.ts
@@ -8,69 +8,77 @@ import CheckUser from '../../auth/checkUser';
 
 export const dynamic = 'force-dynamic'
 export async function GET(request: NextRequest) {
+    try {
         const rawParams = request.url.split('?')[1];
-      const params = qs.parse(rawParams);
-      try{
-        const {db,connection} = await getDb2();
-      var Mainresponse = {};
-      const userid:string = params['id'] as string;
-      const {error,user} = await CheckUser(userid);
-        if(error == true){
-            connection.end();
-            Mainresponse = {
-                status: 'error',
-                error:true,
-                topcategories:[],
-                categories:[],
-                top_places:[],
-                interest_places:[],
-                sponsored_places:[],
-            }
-        }else{
-            var interest_places = [];
-            var interest_categories:number[] = [];
-            // user?.categories.forEach(async (cat,index)=> {
-                
-            //     const ry = await db.select().from(app_top_categories).where(eq(app_top_categories.id,cat));
-            //     if(ry.length > 0){
-            //         ry[0].subCategories.forEach((mcate)=>{
-            //             interest_categories.push(mcate);
-            //         })
-            //     }
-            //     // interest_categories.push()
-            // })
-            const topcate = await db.select().from(app_top_categories);
-            const categ = await db.select().from(app_categories).limit(50);
-            const top_places = await db.select().from(app_place).where(gt(app_place.rating,4)).limit(10);
-            const recently_places = await db.select().from(app_place).orderBy(desc(app_place.id)).limit(10);
-            interest_places = await db.select().from(app_place).where(inArray(
-                app_place.app_category_id,interest_categories)).limit(10);
-            const sponsor_places = await db.select().from(app_place).where(eq(
-                app_place.paid,
-                true
-            )).orderBy(desc(app_place.id)).limit(10);
-
-            connection.end();
-            Mainresponse = {
-                status: 'success', //string
-                topcategories:topcate, //List<topCategoryModel>
-                categories:categ, //List<CategoryModel>
-                top_places:top_places, //List<PlaceModel>
-                recently_added:recently_places, //List<PlaceModel>
-                interest_places:interest_places,//List<PlaceModel>
-                sponsored_places:sponsor_places,//List<PlaceModel>
-                error: false,
-            }
-        }
-     
+        const params = qs.parse(rawParams);
+            const {db,connection} = await getDb2();
+            var Mainresponse = {};
+              const userid:string = params['id'] as string;
+              const {error,user} = await CheckUser(userid);
+            //   return NextResponse.json(user);
+                if(error == true){
+                    connection.end();
+                    Mainresponse = {
+                        status: 'error',
+                        error:true,
+                        topcategories:[],
+                        categories:[],
+                        top_places:[],
+                        interest_places:[],
+                        sponsored_places:[],
+                    }
+                }else{
+                    var interest_places = [];
+                    var interest_categories:number[] = [];
+                    var oneliny = JSON.parse(user?.categories as unknown as string);
+                    const ry = await db.select().from(app_top_categories).where(inArray(app_top_categories.id,oneliny));
+                    ry.forEach(async (cat,index)=> {
+                        var subcate = JSON.parse(cat?.subCategories as unknown as string);
+                        subcate.forEach((mcate:number)=>{
+                                interest_categories.push(mcate);
+                            });
+                    })
+                    // return NextResponse.json(interest_categories);
+                    // return NextResponse.json(JSON.parse(user?.categories as unknown as string))
+                    const topcate = await db.select().from(app_top_categories);
+                    const categ = await db.select().from(app_categories).limit(50);
+                    const top_places = await db.select().from(app_place).where(gt(app_place.rating,4.0)).limit(10);
+                    const recently_places = await db.select().from(app_place).orderBy(desc(app_place.id)).limit(10);
+                    interest_places = await db.select().from(app_place).where(inArray(
+                        app_place.app_category_id,interest_categories)).limit(10);
+                    const sponsor_places = await db.select().from(app_place).where(eq(
+                        app_place.paid,
+                        true
+                    )).orderBy(desc(app_place.id)).limit(10);
+        
+                    connection.end();
+                    Mainresponse = {
+                        status: 'success', //string
+                        topcategories:topcate, //List<topCategoryModel>
+                        categories:categ, //List<CategoryModel>
+                        top_places:top_places, //List<PlaceModel>
+                        recently_added:recently_places, //List<PlaceModel>
+                        interest_places:interest_places,//List<PlaceModel>
+                        sponsored_places:sponsor_places,//List<PlaceModel>
+                        error: false,
+                    }
+                }
         return NextResponse.json(Mainresponse);
-      }catch(e){
-        NextResponse.json({
-            error:true,
-            message:e,
-            data:'error'
-        });
-      }
+
+    }
+    catch(e){
+        return NextResponse.json({
+                    error:true,
+                    message:e,
+                    data:'error'
+                });
+    }
+    //   try{
+    //  
+     
+    //   }catch(e){
+    //     
+    //   }
 }
 
 
